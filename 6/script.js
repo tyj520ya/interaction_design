d0190f519252804c7f5f3f556019afd901e10638
iris <140230751+tyj520ya@users.noreply.github.com>
2024-03-14 21:03:35 +0800

    Add files via upload

---
+++ 6/script.js
@@ -0,0 +1,10 @@
+  function sayHello(){
+            var response = prompt("What is your name?");
+            alert("HELLO,"+response+"have a nice day")
+        }
+
+        function showTopic(){
+            var x=document.getElementById("demo");
+            x.style.fontSize="25px";
+            x.style.color="red"
+        }
